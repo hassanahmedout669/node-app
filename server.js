0caa0ff5ca714fa045ec9b38306469ae3cf19d2b
hassanahmedout669 <98187132+hassanahmedout669@users.noreply.github.com>
2022-04-26 22:42:04 +0500

    Update and rename server.j to server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 style="color:green;"> Rolling Update is working fine , Thanks you AWS !! </h1> \n');
+  res.send('<h1 style="color:green;"> Rolling Update is working fine , Thanks you AWS , Error Fixed !! </h1> \n');
 });
 
 app.listen(PORT, HOST);
