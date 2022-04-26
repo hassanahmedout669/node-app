ba957ab31957a89e06cc2c37dd807ced35166ec7
hassanahmedout669 <98187132+hassanahmedout669@users.noreply.github.com>
2022-04-26 21:34:40 +0500

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 style="color:green;"> Rolling Update is working fine , Thanks !! </h1> \n');
+  res.send('<h1 style="color:green;"> Rolling Update is working fine , Thanks HASSAN !! </h1> \n');
 });
 
 app.listen(PORT, HOST);
